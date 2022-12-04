0d284b9327e53e3e39a4b7eececec99d1dac7bfd
KeryanOulier <72012774+KeryanOulier@users.noreply.github.com>
2022-12-04 18:20:18 +0100

    add return to main menu with space

--- Assets/returnToMenu.cs
+++ Assets/returnToMenu.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class returnToMenu : MonoBehaviour
 {
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-            print("go to menu");
+            SceneManager.LoadScene("MainMenu");
     }
 }
